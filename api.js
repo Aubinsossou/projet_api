2de6103a7875445502c99463ad19d2a26cabf088
Aubin SOSSOU <aubinsossoua@gmail.com>
2025-08-07 22:12:37 +0100

    star add fonctionnality pagination

--- api.js
+++ api.js
@@ -3,6 +3,7 @@ const button_content_2 = document.querySelector(".button_content_2")
 
 const test = document.querySelector(".test")
 
+
 //console.log(button)
 
 
@@ -20,18 +21,23 @@ function api() {
             return response.json(); // Convertit la réponse en JSON
         })
         .then(data => {
-            console.log(data[0])
-            let x=0;
-            const langages = data[x].languages
+           
+            console.log(data[5])
+            const langages = data[0].languages
             const langage = Object.values(langages)
+            const page_actuel_tag=document.querySelector(".page_actuel")
             console.log(langage[0])
-            for(x = 1;x <= 6 ; x++) {
+            let first=0
+            let numb_drapeau=5;
+            let page_actuel=0
+            page_actuel_tag.innerHTML=`${page_actuel+1}`
+            for(x = first;x <=numb_drapeau ; x++) {
                 if (countrie) {
                     content += `
                         <div class="countrie_item">
                             <div class="countrie_item_part1">
                                 <span><img src="${data[x].flags.svg}" alt="" class="img_pays"></span>
-                                <h2 class="name_pays">Pays: ${data[x].continents}</h2><br>
+                                <h2 class="name_pays">Pays: ${data[x].name.common}</h2><br>
                             </div>
                             <div class="countrie_item_part2">
                                 <img src="${data[x].coatOfArms.png}" alt="" class="devise_pays">
@@ -48,25 +54,26 @@ function api() {
                             </div>
                         </div>
                         
-                        `
-                    
+                        `  
                 }
             }
             if (button_content_2) {
                 button_content_2.addEventListener("click", (e) => {
                     e.preventDefault();
-                    console.log("boutton cliqué")
-                    x=x+6
+                    console.log(first+=5, numb_drapeau+=5,page_actuel+=1)
+                    
                 })
 
             }
-                    countrie.innerHTML = content
+                    
 
             if (button_content_1) {
-                button_content_1.addEventListener("click", () => {
-
+                button_content_1.addEventListener("click", (e) => {
+                     e.preventDefault();
+                    console.log(first-=5, numb_drapeau-=5,page_actuel-=1)
                 })
             }
+            countrie.innerHTML = content
 
 
         })
