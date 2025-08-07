1ab87ceb3e91e598956658d048d117dd63ff14d7
Aubin SOSSOU <aubinsossoua@gmail.com>
2025-08-07 23:51:31 +0100

    still fonctionnality pagination

--- api.js
+++ api.js
@@ -1,8 +1,10 @@
 const button_content_1 = document.querySelector(".button_content_1")
 const button_content_2 = document.querySelector(".button_content_2")
-
+const page_actuel_tag = document.querySelector(".page_actuel")
 const test = document.querySelector(".test")
-
+let first = 0
+let numb_drapeau = 5;
+let page_actuel = 1
 
 //console.log(button)
 
@@ -21,17 +23,16 @@ function api() {
             return response.json(); // Convertit la réponse en JSON
         })
         .then(data => {
-           
-            console.log(data[5])
+
+            console.log(data.length)
             const langages = data[0].languages
             const langage = Object.values(langages)
-            const page_actuel_tag=document.querySelector(".page_actuel")
+            
             console.log(langage[0])
-            let first=0
-            let numb_drapeau=5;
-            let page_actuel=0
-            page_actuel_tag.innerHTML=`${page_actuel+1}`
-            for(x = first;x <=numb_drapeau ; x++) {
+
+
+            page_actuel_tag.innerHTML = `${page_actuel}`
+            for (x = first; x <= numb_drapeau; x++) {
                 if (countrie) {
                     content += `
                         <div class="countrie_item">
@@ -54,28 +55,29 @@ function api() {
                             </div>
                         </div>
                         
-                        `  
+                        `
                 }
+            }
+           
+        
+        countrie.innerHTML = content
+        
+        
+    })
+     if (button_content_1) {
+                button_content_1.addEventListener("click", (e) => {
+                    e.preventDefault();
+                    first -= 5, numb_drapeau -= 5, page_actuel -= 1
+                    api()
+                })
             }
             if (button_content_2) {
                 button_content_2.addEventListener("click", (e) => {
                     e.preventDefault();
-                    console.log(first+=5, numb_drapeau+=5,page_actuel+=1)
-                    
+                    first += 5, numb_drapeau += 5, page_actuel += 1
+                    api()
                 })
-
-            }
-                    
-
-            if (button_content_1) {
-                button_content_1.addEventListener("click", (e) => {
-                     e.preventDefault();
-                    console.log(first-=5, numb_drapeau-=5,page_actuel-=1)
-                })
-            }
-            countrie.innerHTML = content
-
-
-        })
+            
+        }
 }
 api()
